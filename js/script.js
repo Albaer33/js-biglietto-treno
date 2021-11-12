b182abb0f18c999e4b511c63ca32e0c21766587e
Albaer33 <alisi3396@gmail.com>
2021-11-12 15:03:34 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // dichiarazione variabili
-const kilometers = prompt('quanti kilometri vuoi percorrere?')
-const userAge = prompt('inserisci la tua età:')
+const kilometers = parseInt(prompt('quanti kilometri vuoi percorrere?'));
+const userAge = parseInt(prompt('inserisci la tua età:'));
+const pricePerKilometer = 0.21;
+let totalPrice;
+
 // esecuzione
+totalPrice = kilometers * pricePerKilometer;
+// applicazione sconto
+if (userAge < 18) {
+    let discount = (totalPrice * 20) / 100;
+    totalPrice = totalPrice - discount;
+}
+else if (userAge > 65) {
+    let discount = (totalPrice * 40) / 100;
+    totalPrice = totalPrice - discount;
+}
 // output
+alert('il costo totale del biglietto è di: ' + totalPrice.toFixed(2) + ' euro');
